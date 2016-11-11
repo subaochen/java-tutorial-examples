ffbe2516be1d408badfa06bccd6aebbd82e93f9a
subaochen <subaochen@126.com>
2016-11-11 20:20:05 +0800

    增加接口相关示例

---
+++ oopm/src/cn/edu/sdut/softlab/oopm/pureabstract/Dog.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016 Su Baochen and individual contributors by the 
+ * @authors tag. See the copyright.txt in the distribution for
+ * a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.edu.sdut.softlab.oopm.pureabstract;
+
+/**
+ * 本类演示了如何实现接口.
+ *
+ * @author Su Baochen
+ */
+public class Dog implements Animal {
+
+  @Override
+  public void hello() {
+    System.out.println("汪汪");
+  }
+
+  @Override
+  public void eat() {
+    System.out.println("Dog eat method");
+  }
+
+  @Override
+  public void sleep() {
+    System.out.println("Dog sleep method");
+  }
+
+}
