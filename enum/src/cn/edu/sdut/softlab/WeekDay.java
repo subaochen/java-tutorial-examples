c3655fc8f5aed7b3638275740ad78f170df8bc56
Su Baochen <subaochen@126.com>
2016-12-21 11:49:47 +0800

    准备Enum示例

--- enum/src/cn/edu/sdut/softlab/WeekDay.java
+++ enum/src/cn/edu/sdut/softlab/WeekDay.java
@@ -3,6 +3,6 @@ package cn.edu.sdut.softlab;
 /**
  * Created by subaochen on 16-12-20.
  */
-public enum Week {
+public enum WeekDay {
     SUNDAY,MONDAY,TUESDAY,WENDESDAY,THURSDAY,FRIDAY,SATURDAY;
 }
