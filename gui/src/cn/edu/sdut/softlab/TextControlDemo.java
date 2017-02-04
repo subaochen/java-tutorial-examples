679ccc8a2c9cecde790ccd0d2ec9a5c90038c8eb
su baochen <subaochen@126.com>
2017-02-04 08:05:59 +0800

    new examples for gui

--- gui/src/cn/edu/sdut/softlab/TextControlDemo.java
+++ gui/src/cn/edu/sdut/softlab/TextControlDemo.java
@@ -1,9 +1,24 @@
 package cn.edu.sdut.softlab;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by subaochen on 17-2-3.
  */
 public class TextControlDemo {
+    private JTextField textField1;
+    private JFormattedTextField formattedTextField1;
+    private JPasswordField passwordField1;
+    private JLabel typedContent;
+
+    public TextControlDemo() {
+        textField1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+    }
 }
