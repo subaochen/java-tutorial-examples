b43a3258767b4a8a658243de401db59c0f041a37
subaochen <subaochen@126.com>
2017-02-04 17:14:14 +0800

    continue example of gui text controls

--- gui/src/cn/edu/sdut/softlab/TextControlDemo.java
+++ gui/src/cn/edu/sdut/softlab/TextControlDemo.java
@@ -1,8 +1,8 @@
 package cn.edu.sdut.softlab;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 /**
  * Created by subaochen on 17-2-3.
@@ -12,13 +12,41 @@ public class TextControlDemo {
     private JFormattedTextField formattedTextField1;
     private JPasswordField passwordField1;
     private JLabel typedContent;
+    private JPanel mainPanel;
+
 
     public TextControlDemo() {
-        textField1.addActionListener(new ActionListener() {
+        // 这里只是监听了失去焦点的事件：当失去焦点时，在最下面的Label区域显示该输入框的当前内容
+        textField1.addFocusListener(new FocusAdapter() {
             @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-
+            public void focusLost(FocusEvent focusEvent) {
+                super.focusLost(focusEvent);
+                typedContent.setText("输入的内容为：" + textField1.getText());
             }
         });
+        formattedTextField1.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent focusEvent) {
+                super.focusLost(focusEvent);
+                // @TODO 没有验证输入内容的格式是否合法
+                typedContent.setText("输入的内容为：" + formattedTextField1.getText());
+            }
+        });
+        passwordField1.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent focusEvent) {
+                super.focusLost(focusEvent);
+                // @TODO getText为过时的方法
+                typedContent.setText("输入的内容为：" + passwordField1.getText());
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("TextControlDemo");
+        frame.setContentPane(new TextControlDemo().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
