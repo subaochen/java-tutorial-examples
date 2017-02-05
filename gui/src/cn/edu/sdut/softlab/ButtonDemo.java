aaf395d101d0850815e861cac2bdc9829a310c63
su baochen <subaochen@126.com>
2017-02-05 09:20:05 +0800

    new example for java gui

---
+++ gui/src/cn/edu/sdut/softlab/ButtonDemo.java
@@ -0,0 +1,13 @@
+package cn.edu.sdut.softlab;
+
+import javax.swing.*;
+
+/**
+ * Created by subaochen on 17-2-4.
+ */
+public class ButtonDemo {
+    private JPanel mainPanel;
+    private JButton emegencyCallButton;
+    private JButton searchButton;
+    private JButton disabledButton;
+}
