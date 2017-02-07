4f03f14d5b7603549c3279c9ae74351ee33b2a1d
subaochen <subaochen@126.com>
2017-02-07 16:57:55 +0800

    new example for gui layout

---
+++ gui/src/cn/edu/sdut/softlab/TestDemo.java
@@ -0,0 +1,12 @@
+package cn.edu.sdut.softlab;
+
+import javax.swing.*;
+
+/**
+ * Created by subaochen on 17-2-7.
+ */
+public class TestDemo {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+}
