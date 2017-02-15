48a3c77ae1a9eea17740f9842a9d748d1cda27d0
subaochen <subaochen@126.com>
2017-02-15 16:53:31 +0800

    完成多线程示例

--- concurrent/src/cn/edu/sdut/softlab/sync/Client.java
+++ concurrent/src/cn/edu/sdut/softlab/sync/Client.java
@@ -1,18 +1,20 @@
-package cn.edu.sdut.softlab.unsynced;
+package cn.edu.sdut.softlab.sync;
 
 /**
  * Created by subaochen on 17-2-13.
  */
 public class Client {
     public static void main(String[] args) throws InterruptedException {
-        Box box = new Box();
-        Producer p = new Producer(box);
+        Buffer buffer = new Buffer();
+        Producer p = new Producer(buffer);
         Thread pt = new Thread(p);
         pt.start();
-        Consumer c = new Consumer(box);
+
+        Consumer c = new Consumer(buffer);
         Thread ct = new Thread(c);
         ct.start();
-        Thread.sleep(5000);
+
+        Thread.sleep(5000); // 让两个线程最多运行5s
         pt.interrupt();
         ct.interrupt();
     }
