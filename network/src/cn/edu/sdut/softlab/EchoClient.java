71fc92f32309bf0c8c62178e384381f3392fef43
subaochen <subaochen@126.com>
2017-02-18 11:45:10 +0800

    增加EchoServer/EchoClient的若干注释

--- network/src/cn/edu/sdut/softlab/EchoClient.java
+++ network/src/cn/edu/sdut/softlab/EchoClient.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 /**
  * Created by subaochen on 17-2-17.
@@ -23,28 +24,24 @@ public class EchoClient {
         int portNumber = Integer.parseInt(args[1]);
 
         try (
-                Socket echoSocket = new Socket(hostName, portNumber);
-                PrintWriter out =
-                        new PrintWriter(echoSocket.getOutputStream(), true);
+                Socket echoSocket = new Socket(hostName, portNumber); // |\longremark{根据socket server的主机IP地址和端口号创建一个到socket server的socket对象}|
+                PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true); // |\longremark{获得写入到服务器端的OutputStream}|
                 BufferedReader in =
                         new BufferedReader(
-                                new InputStreamReader(echoSocket.getInputStream()));
-                BufferedReader stdIn =
-                        new BufferedReader(
-                                new InputStreamReader(System.in))
+                                new InputStreamReader(echoSocket.getInputStream())); // |\longremark{获得从服务器读取数据的InputStream}|
+                Scanner stdIn = new Scanner(System.in); // |\longremark{标准输入}|
         ) {
             String userInput;
-            while ((userInput = stdIn.readLine()) != null) {
-                out.println(userInput);
-                System.out.println("echo: " + in.readLine());
+            while ((userInput = stdIn.nextLine()) != null) {
+                out.println(userInput); // |\longremark{写入到服务器端}|
+                System.out.println("echo: " + in.readLine()); // |\longremark{读取服务器端的响应并显示在屏幕上}|
             }
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + hostName);
             System.exit(1);
         } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                    hostName);
+            System.err.println("Couldn't get I/O for the connection to " + hostName);
             System.exit(1);
         }
     }
-}
+}//|\showremarks|
--- network/src/cn/edu/sdut/softlab/EchoServer.java
+++ network/src/cn/edu/sdut/softlab/EchoServer.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * Created by subaochen on 17-2-17.
@@ -21,17 +22,15 @@ public class EchoServer {
         int portNumber = Integer.parseInt(args[0]);
 
         try (
-                ServerSocket serverSocket =
-                        new ServerSocket(Integer.parseInt(args[0]));
-                Socket clientSocket = serverSocket.accept();
-                PrintWriter out =
-                        new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(clientSocket.getInputStream()));
+                ServerSocket serverSocket = new ServerSocket(portNumber); //|\longremark{创建ServerSocket对象}|
+                Socket clientSocket = serverSocket.accept(); // |\longremark{等待客户端发起连接请求}|
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); // |\longremark{获得写入客户端的outputStream}|
+                Scanner in = new Scanner(clientSocket.getInputStream()); //|\longremark{获得从客户端读取数据的inputStream}|
         ) {
             String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                out.println(inputLine);
+            while ((inputLine = in.nextLine()) != null) { // |\longremark{从客户端读取一行，如果存在的话}|
+                System.out.println("from client:" + inputLine);
+                out.println(inputLine); // |\longremark{将从客户端读取的一行字符串发送回客户端}|
             }
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port "
@@ -39,4 +38,4 @@ public class EchoServer {
             System.out.println(e.getMessage());
         }
     }
-}
+} // |\showremarks|
