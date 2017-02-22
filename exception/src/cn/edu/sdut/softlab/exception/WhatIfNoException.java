e50b8d82baab7abe77091761102507b5caddde00
subaochen <subaochen@126.com>
2017-02-22 22:59:28 +0800

    去掉版权信息

--- exception/src/cn/edu/sdut/softlab/exception/WhatIfNoException.java
+++ exception/src/cn/edu/sdut/softlab/exception/WhatIfNoException.java
@@ -3,18 +3,8 @@ package cn.edu.sdut.softlab.exception;
 import java.time.LocalDate;
 import java.util.Scanner;
 
-/**
- * 本类演示了Java的异常处理机制 .
- *
- * @author Su Baochen
- */
 public class WhatIfNoException {
 
-  /**
-   * 程序执行入口.
-   *
-   * @param args 命令行参数
-   */
   public static void main(String[] args) {
     Scanner console = new Scanner(System.in);
     System.out.print("Please input date:");
