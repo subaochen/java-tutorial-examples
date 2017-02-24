ed2e7fc4c8a3669ba62af11bb3cc1aa3c77a79c1
subaochen <subaochen@126.com>
2017-02-24 17:08:09 +0800

    适当增加注释

--- verybasic/src/Month.java
+++ verybasic/src/Month.java
@@ -12,8 +12,8 @@ public class Month {
     int days = 0;
 
     Scanner console = new Scanner(System.in);
-    System.out.print("请输入月份的英文缩写：");
-    String month = console.next();
+    System.out.print("请输入月份的英文缩写："); //|\longremark{注意到，这里需要紧跟着“：”后面输入月份的英文缩写，而不是回车再输入}|
+    String month = console.next();//|\longremark{从键盘读入下一个标识符，使用空格、Tab或者回车隔开各个标识符}|
     switch (month) {
       case "FEB": // |\longremark{在这里故意忽略了闰年问题，你能修改一下这个程序，解决闰年问题吗？}|
         days = 28;
@@ -26,7 +26,7 @@ public class Month {
       case "OCT":
       case "DEC":
         days = 31;
-        break;
+        break;　//|\longremark{这里的break不要忘记}|
       case "APR":
       case "JUN":
       case "SEPT":
